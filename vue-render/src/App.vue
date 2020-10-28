4687dc30016859ee9997a43c0904a9b0c3953aa5
JelmerVeen <paluke_@hotmail.com>
2020-10-28 20:04:12 +0100

    Created a Blazor, Vue and Two vanillaJS implementations to render 1000 rows in a table, to see which is fastest

---
+++ vue-render/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>SepalLength</th>
+          <th>SepalWidth</th>
+          <th>PetalLength</th>
+          <th>PetalWidth</th>
+          <th>Species</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="flower.species" v-for="flower in irisSet">
+          <td>{{ flower.sepalLength }}</td>
+          <td>{{ flower.sepalWidth }}</td>
+          <td>{{ flower.petalLength }}</td>
+          <td>{{ flower.petalWidth }}</td>
+          <td>{{ flower.species }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RenderTest",
+  data() {
+    return {
+      dataSet: {},
+    };
+  },
+  computed: {
+    irisSet() {
+      return this.dataSet;
+    },
+  },
+  created() {
+    fetch("/dataset/mockIrisSet.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        this.dataSet = json;
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
